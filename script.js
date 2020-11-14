456b738f93b4b9018591ba4167f6b4c91703e3bd
Nick Johnson <nicklynnjohnson@gmail.com>
2020-11-13 18:50:38 -0800

    Trying to get functions to work within Ajax call.

--- script.js
+++ script.js
@@ -1,57 +1,13 @@
 
 //Declare variables
-//var city = "";
+var currentCity = $("#current-city");
 
-
-//Example city
 var city = "los angeles";
-
-
-//City array (make it a global variable) prepopulated with Los Angeles
-
-// Click event for search box
-//Input box for specific city
-
-
 
 var listOfCities = ["Los Angeles"];
 
 
-//API Key used for both current (including additional UV query) and forecast weather functions below
 
-
-
-
-//Function for getting the current weather
-
-
-//get lat lon
-
-
-
-
-
-
-
-// function getCityCoordinates(city) {
-   
-//     var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
-
-
-
-//     $.ajax({
-//         url: queryURL,
-//         method: "GET"
-
-//     }).then(function(response){
-//         console.log(response);
-
-//         insideCityCoordinates = {
-//             cityLat: response.coord.lat,
-//             cityLon: response.coord.lon
-//         }
-//     });
-// }
 
 var APIKey = "479089a9990486902ba42a70c1a35171";
 
@@ -72,7 +28,7 @@ function displayAllWeather(city) {
 
         // Second, call OpenWeatherMap's "One Call API" by that city's Lat and Lon coordinates to get both the current and forecast weather data
 
-        var queryURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${cityLat}&lon=${cityLon}&units=imperial&exclude=minutely,hourly,daily,alerts&appid=${APIKey}`;
+        var queryURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${cityLat}&lon=${cityLon}&units=imperial&exclude=minutely,hourly,alerts&appid=${APIKey}`;
 
         $.ajax({
             url: queryURL,
@@ -81,84 +37,27 @@ function displayAllWeather(city) {
             }).then(function(response2){
                 console.log(response2);
 
-                displayCurrentWeather();
-                displayForecastWeather();
+                displayCurrentWeather(response2);
+                displayForecastWeather(response2);
             });
     });
 }
 
-function displayCurrentWeather() {
+function displayCurrentWeather(response2) {
+    // Note: The current city's cityName is passed through from Response 1 API
+    var chosenCity = response1.main;
+    var unixCurrentDate = moment.unix(response2.current.dt);
+    var currentDate = unixCurrentDate.format("M/D/YYYY");
+    var currentIcon = $("<img>").attr("src", "http://openweathermap.org/img/wn/" + response2.current.weather[0].icon + ".png");
+
+    $(currentCity).html(chosenCity + currentDate + currentIcon); 
 
 }
 
-function displayForecastWeather() {
+function displayForecastWeather(response2) {
 
 }
-
-
-
-
-
 
 
 displayAllWeather(city);
 
-
-
-// function getCityWeather(cityLat, cityLon) {
-//     var queryURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${cityLat}&lon=${cityLon}&units=imperial&exclude=minutely,hourly,daily,alerts&appid=${APIKey}`;
-
-//     $.ajax({
-//         url: queryURL,
-//         method: "GET"
-
-//     }).then(function(response){
-//         console.log(response);
-
-//     });
-// }
-
-//getCityCoordinates(city);
-
-// function displayAllWeather(city) {
-//     getCityCoordinates(city);
-//     var outsideCityCoordinates = getCityCoordinates(city);
-//     getCityWeather(cityLat, cityLon);
-// }
-
-
-// Action: Click Handlers
-//$("#search-button").on("click", displayAllWeather);
-
-
-
-//http errors 
-
-//get weather data one call
-
-//getCityCoordinates(city);
-
-//Function for getting the forecast weather
-//function getWeatherForecast
-
-
-//color code UV index 3 or less etc. 4 - 8  (bootstrap colors)
-
-
-
-//function called to Empty Div first iterate in city history array create UL tag, as iterate create li item (button or list item) 1) check if cities in local storage, target searchedCities 
-//ul has class / append li to that class / li need an id of the city / outside of function another click event where target class ul
-//target id .val.trim
-
-//weather icon concatenated with string .png
-
-
-//date in Unix therefore use moment
-
-//exlude minutely, alerts, etc.
-
-//units: choose imperial
-
-// &units=imperial
-
-// var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
