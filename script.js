edd59bd0861a19aa4911444d2e4088edd3643db5
Nick Johnson <nicklynnjohnson@gmail.com>
2020-11-15 19:27:39 -0800

    Dynamically generate forecast cards. Fill forecast cards with data.

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 
-//Declare variables
+// Declare variables
+// Current city/date variables
 var currentCity = $("#current-city");
+var currentTemp = $("#current-temp");
+var currentHumid = $("#current-humid");
+var currentWind = $("#current-wind");
+var currentUV = $("#current-uv");
 
-var city = "los angeles";
+
+// Forecast date variables
+var forecastRow = $(".forecast-row");
+
+
+
+
+
+var city = "seattle";
 
 var listOfCities = ["Los Angeles"];
 
@@ -23,6 +36,7 @@ function displayAllWeather(city) {
     }).then(function(response1){
         console.log(response1);
 
+        var chosenCity = response1.name;
         var cityLat = response1.coord.lat;
         var cityLon = response1.coord.lon;
 
@@ -34,29 +48,63 @@ function displayAllWeather(city) {
             url: queryURL,
             method: "GET"
             
-            }).then(function(response2){
-                console.log(response2);
+        }).then(function(response2) {
+            console.log(response2);
 
-                displayCurrentWeather(response2);
-                displayForecastWeather(response2);
-            });
+            // (1/2) Display Weather: Current
+
+            var unixCurrentDate = moment.unix(response2.current.dt);
+            var currentDate = unixCurrentDate.format("MM/DD/YYYY");
+            var currentIcon = $("<img>").attr("src", "http://openweathermap.org/img/wn/" + response2.current.weather[0].icon + "@2x.png");
+
+            $(currentCity).html(chosenCity + " " + currentDate + " " + currentIcon); 
+            $(currentTemp).html(response2.current.temp + "&#8457");
+            $(currentHumid).html(response2.current.humidity + "%");
+            $(currentWind).html(response2.current.wind_speed + "MPH");
+
+            var currentUVIndex = response2.current.uvi;
+
+            $(currentUV).html(currentUVIndex);
+
+                // UV Index scale source: https://en.wikipedia.org/wiki/Ultraviolet_index
+                if (currentUVIndex >= 0 && currentUVIndex <= 2.9) {
+                    $(currentUV).attr("class", "green");
+                } else if (currentUVIndex >= 3 && currentUVIndex <= 5.9) {
+                    $(currentUV).attr("class", "yellow");
+                } else if (currentUVIndex >= 6 && currentUVIndex <= 7.9) {
+                    $(currentUV).attr("class", "orange");
+                } else if (currentUVIndex >= 8 && currentUVIndex <= 10.9) {
+                    $(currentUV).attr("class", "red");
+                } else {
+                    $(currentUV).attr("class", "violet");
+                }
+
+            // (2/2) Display Weather: Forecast
+            for (var i = 0; i < 5; i++) {
+                // Create the card
+                var forecastCol = $("<div class='col-2'>");
+                forecastRow.append(forecastCol);
+                var forecastCard = $("<div class='card text-white bg-primary'>");
+                forecastCol.append(forecastCard);
+                var forecastCardBody = $("<div class='card-body'>");
+                forecastCard.append(forecastCardBody);
+
+                // Fill the card
+                var unixForecastDate = moment.unix(response2.daily[i].dt);
+                var forecastDate = unixForecastDate.format("MM/DD/YYYY");
+                var forecastCardTitle = $("<h5 class='card-title'>").text(forecastDate);
+
+                var forecastIcon = $("<img>").attr("src", "http://openweathermap.org/img/wn/" + response2.daily[i].weather[0].icon + ".png");
+                var forecastTempDay = $("<p class='card-text'>").html("Temp:" + response2.daily[i].temp.day + "&#8457");
+                var forecastHumid = $("<p class='card-text'>").html("Humidity:" + response2.daily[i].humidity + "%");
+                
+                forecastCardBody.append(forecastCardTitle, forecastIcon, forecastTempDay, forecastHumid);
+            }
+        });
     });
 }
 
-function displayCurrentWeather(response2) {
-    // Note: The current city's cityName is passed through from Response 1 API
-    var chosenCity = response1.main;
-    var unixCurrentDate = moment.unix(response2.current.dt);
-    var currentDate = unixCurrentDate.format("M/D/YYYY");
-    var currentIcon = $("<img>").attr("src", "http://openweathermap.org/img/wn/" + response2.current.weather[0].icon + ".png");
 
-    $(currentCity).html(chosenCity + currentDate + currentIcon); 
-
-}
-
-function displayForecastWeather(response2) {
-
-}
 
 
 displayAllWeather(city);
