59074bfc42f3bb6c055bfa861b62421393617a41
Nick Johnson <nicklynnjohnson@gmail.com>
2020-11-28 17:02:29 -0800

    Finished searched city button list.

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,32 +1,34 @@
 
 // Declare variables
 // Current city/date variables
+var currentDiv = $(".current-div");
 var currentCity = $("#current-city");
 var currentTemp = $("#current-temp");
 var currentHumid = $("#current-humid");
 var currentWind = $("#current-wind");
 var currentUV = $("#current-uv");
+var currentImage = $("#current-image");
+
+// Start with Los Angeles already included in the array so that the user sees how the dashboard works
+//var listOfCities = ["Los Angeles"];
+var listOfCities = [];
+//var cityList = listOfCities;
+//localStorage.setItem("listOfCities", JSON.stringify(cityList));
+var buttonsList = $("#buttons-list");
+
 
 
 // Forecast date variables
 var forecastRow = $(".forecast-row");
 
-
-
-
-
-var city = "seattle";
-
-var listOfCities = ["Los Angeles"];
-
-
-
-
+// API calls section
 var APIKey = "479089a9990486902ba42a70c1a35171";
 
-function displayAllWeather(city) {
+function displayAllWeather(passedCity) {
 
     // First, call OpenWeatherMap's "Current Weather Data"  by city to get that city's Lat and Lon coordinates
+
+    var city = passedCity;
     var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
 
     $.ajax({
@@ -39,6 +41,7 @@ function displayAllWeather(city) {
         var chosenCity = response1.name;
         var cityLat = response1.coord.lat;
         var cityLon = response1.coord.lon;
+
 
         // Second, call OpenWeatherMap's "One Call API" by that city's Lat and Lon coordinates to get both the current and forecast weather data
 
@@ -55,9 +58,11 @@ function displayAllWeather(city) {
 
             var unixCurrentDate = moment.unix(response2.current.dt);
             var currentDate = unixCurrentDate.format("MM/DD/YYYY");
-            var currentIcon = $("<img>").attr("src", "http://openweathermap.org/img/wn/" + response2.current.weather[0].icon + "@2x.png");
 
-            $(currentCity).html(chosenCity + " " + currentDate + " " + currentIcon); 
+            $(currentImage).attr("src", "http://openweathermap.org/img/wn/" + response2.current.weather[0].icon + "@2x.png");
+
+            $(currentCity).html(chosenCity + " " + currentDate); 
+            currentCity.append(currentImage);
             $(currentTemp).html(response2.current.temp + "&#8457");
             $(currentHumid).html(response2.current.humidity + "%");
             $(currentWind).html(response2.current.wind_speed + "MPH");
@@ -80,7 +85,7 @@ function displayAllWeather(city) {
                 }
 
             // (2/2) Display Weather: Forecast
-            for (var i = 0; i < 5; i++) {
+            for (var i = 1; i < 6; i++) {
                 // Create the card
                 var forecastCol = $("<div class='col-2'>");
                 forecastRow.append(forecastCol);
@@ -104,8 +109,49 @@ function displayAllWeather(city) {
     });
 }
 
+// Search for a city section
+
+// (1/3) Populate the list with historical city searches
+function displayButtons() {
+    buttonsList.empty();
+
+    var enteredCity = localStorage.getItem("listOfCities");
+    enteredCity = JSON.parse(enteredCity);
+
+    if (enteredCity.length > 0) {
+        displayAllWeather(enteredCity[enteredCity.length-1]);
+    }
+
+    for (var i = 0; i < enteredCity.length; i++) {
+        listButton = $("<button class='stored-city list-group-item'>").attr("data-name", enteredCity[i]).text(enteredCity[i]);
+        buttonsList.prepend(listButton);  
+    }
+
+    var cityList = enteredCity;
+    localStorage.setItem("listOfCities", JSON.stringify(cityList));
+}
+
+// (2/3) Store a city
+
+$("#add-city").on("click", function(event) {
+    event.preventDefault();
+
+    var city = $("#city-entry").val().trim();
+
+    listOfCities.push(city);
+
+    var cityList = listOfCities;
+    localStorage.setItem("listOfCities", JSON.stringify(cityList))
+
+    displayButtons();
+})
+
+// (3/3) Make the searched cities in the list clickable/searchable
+
+$(document).on("click", "stored-city", displayAllWeather);
+
+// Initiate the webpage
+displayButtons();
 
 
-
-displayAllWeather(city);
 
